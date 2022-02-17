9ae538cfb191dcebb8a63e2239aee1c495e94918
sam1201 <tuladharsamyak@gmail.com>
2022-02-17 17:10:56 +0545

    token generated after successful authentication

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -1,5 +1,8 @@
 const { User } = require('~models');
-
+const jwt = require('jsonwebtoken');
+const { response } = require('express');
+const { comparePassword } = require('~utility/password');
+const app_secret = process.env.SECRET_KEY
 class UserController {
 
     async register(req, res) {
@@ -10,20 +13,57 @@ class UserController {
                 message:"User Created Successfully."
             })
         } catch (error) {
-            console.log({error})
             return res.status(500).json({ 
                 message: "Server error"
             })
         }
     }
 
+    generateToken(payload) {
+        return 
+    }
     async login(req,res) {
+
+        const { email,password } = req.body;
         try {
+
+            const user = await User.findOne({
+                email
+            })
+
             
+            if(!user) {
+                return res.status(422).json({
+                    message: 'invalid email or password'
+                });
+            }
+
+            if(!comparePassword(password,user.password)) {
+                return res.status(422).json({
+                    message: 'invalid email or password'
+                });
+            }
+            
+            const { name } = user;
+            const token = jwt.sign({
+                payload: {
+                    name
+                }
+            },app_secret,{
+                expiresIn: process.env.EXPIRES_IN
+            })
+
+            return res.json({
+                token
+            })
         } catch (error) {
-            
+            console.log(error)
+            return res.status(500).json({
+                error
+            })
         }
     }
+
 }
 
 module.exports = new UserController
